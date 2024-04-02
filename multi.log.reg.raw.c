a518184e93e7ca91228c6e9f4ac5b65ad74e2005
Laszlo Varga <varga.laszlo.drz@gmail.com>
2024-04-02 10:00:24 +0200

    the raw file is ready.

--- multi.log.reg.raw.c
+++ multi.log.reg.raw.c
@@ -3,14 +3,6 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define LEARNING_RATE 0.01
-#define MAX_ITERATIONS 10000
-#define NUM_CLASSES 2
-#define NUM_FEATURES 3
-#define NUM_SAMPLES 6
-
-/* multiLogR() */
-
 typedef struct Setup {
     double learningRate;
     unsigned int maxIteration;
@@ -70,17 +62,6 @@ bool multiLogRPushWeight(Data *d, double w) {
     d->wCounter++;
 }
 
-void train(double X[NUM_SAMPLES][NUM_FEATURES], int y[NUM_SAMPLES], double weights[NUM_FEATURES], int num_iterations) {
-    for (int iter = 0; iter < num_iterations; ++iter) {
-        for (int i = 0; i < NUM_SAMPLES; ++i) {
-            double prediction = calcSigmoid(calcDotProduct(weights, X[i], NUM_FEATURES));
-            for (int j = 0; j < NUM_FEATURES; ++j) {
-                weights[j] += LEARNING_RATE * (y[i] - prediction) * X[i][j];
-            }
-        }
-    }
-}
-
 bool multiLogRTrain(Setup *s, Data *d) {
     for (int iteration = 0 ; iteration < s->maxIteration ; ++iteration)
         for (int i = 0; i < d->sampleCounter; ++i) {
@@ -92,12 +73,7 @@ bool multiLogRTrain(Setup *s, Data *d) {
     return true;
 }
 
-int old(double x[NUM_FEATURES], double weights[NUM_FEATURES]) {
-    double p = calcSigmoid(calcDotProduct(weights, x, NUM_FEATURES));
-    return (p >= 0.5) ? 1 : 0;
-}
-
-int multiLogRPredict(Setup *s, Data *d, double *inputData /*double x[NUM_FEATURES], double weights[NUM_FEATURES]*/) {
+int multiLogRPredict(Setup *s, Data *d, double *inputData) {
     double p = calcSigmoid(calcDotProduct(d->weight, inputData, s->featureSize));
     return (p >= 0.5) ? 1 : 0;
 }
@@ -108,59 +84,53 @@ int main() {
     Setup setup;
     Result result;
 
-    // test datas
-    double X[NUM_SAMPLES][NUM_FEATURES] = {{2.0, 1.0, 1.0},
-                                           {3.0, 2.0, 0.0},
-                                           {3.0, 4.0, 0.0},
-                                           {5.0, 5.0, 1.0},
-                                           {7.0, 5.0, 1.0},
-                                           {2.0, 5.0, 1.0}};
+    // test datas  
+    #define SAMPLES 6
+    #define FEATURES 3
+    
+    double X[SAMPLES][FEATURES] = 
+		{{2.0, 1.0, 1.0},
+         {3.0, 2.0, 0.0},
+         {3.0, 4.0, 0.0},
+         {5.0, 5.0, 1.0},
+         {7.0, 5.0, 1.0},
+         {2.0, 5.0, 1.0}
+		};
 
-    int Y[NUM_SAMPLES] = {0, 0, 0, 1, 1, 1}; // Osztálycímkék
-    double weights[NUM_FEATURES] = {0.0,0.0,0.0}; // Súlyok inicializálása
+    int Y[SAMPLES] =
+		{0, 0, 0, 1, 1, 1}; 
+			
+    double weights[FEATURES] =
+		{0.0,0.0,0.0}; 
 
-
-
+	// setup
     setup.maxIteration = 10000;
     setup.learningRate = 0.01;
 
-    setup.sampleSize = NUM_SAMPLES;
-    setup.featureSize = NUM_FEATURES;
-
+    setup.sampleSize = SAMPLES;
+    setup.featureSize = FEATURES;
 
     // init
     multiLogRInit(&setup, &data, &result);
 
     // push (training) datas
-    for (int i = 0; i < NUM_SAMPLES ; i++)
+    for (int i = 0; i < setup.sampleSize ; i++)
         multiLogRPush(&data, X[i], Y[i]);
 
-    for (int i = 0; i < NUM_FEATURES ; i++)
+    for (int i = 0; i < setup.featureSize ; i++)
         multiLogRPushWeight(&data, weights[i]);
 
     // train
-    train(X, Y, weights, MAX_ITERATIONS);
     multiLogRTrain(&setup, &data);
 
-    printf ("Regi sulyok: \n");
-    for (int i = 0 ; i < sizeof(weights)/sizeof(double) ; i++) {
-        printf ("%lf ", weights[i]);
-    }
-    printf ("\n");
-
-    printf ("Uj sulyok: \n");
-    for (int i = 0 ; i < 3; i++) {
-        printf ("%lf ", data.weight[i]);
-    }
-    printf ("\n");
-
     // check
-    double input_data[NUM_FEATURES] = {2.2, 4.9, 1.8};
+    double input_data[FEATURES] = {2.2, 4.9, 1.8};
 
     // predection
-    int prediction = old(input_data, weights);
     result.result = multiLogRPredict(&setup, &data, input_data);
-    printf("Prediction: %d - Regi: %d\n", result.result, prediction);
+    
+    // show the result
+    printf("The prediction is %d with the (%.1lf %.1lf %.1lf) input numbers.\n", result.result, input_data[0], input_data[1], input_data[2]);
 
     return 0;
 }
