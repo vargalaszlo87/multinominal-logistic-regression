0aaa499e704b678bb09dcb5f6717ac5da342f2eb
Laszlo Varga <varga.laszlo.drz@gmail.com>
2024-04-09 23:06:51 +0200

    add accuracy and loss

--- multi.log.reg.raw.c
+++ multi.log.reg.raw.c
@@ -23,22 +23,25 @@ typedef struct Stack {
 
 typedef struct XY {
     double **x;
-    unsigned int *y;		
+    unsigned int *y;
+	unsigned int counter;		
 } XY;
+
+typedef struct W {
+    double *w;
+    unsigned int counter;	
+} W;
 
 typedef struct Data  {
 	XY training;
  	XY validation;
-    double *weight;
-    unsigned int sampleCounter;
-    unsigned int weightCounter;
-    Stack validationIndex;
+ 	W weight;
+	Stack validationIndex;
     Setup setup;
-} Data, *pData;
-
-typedef struct Result {
+    double accuracy;
+    double loss;
     int result;
-} Result, *pResult;
+} Data, *pData;
 
 // stack
 
@@ -80,7 +83,7 @@ double calcBoXMuller() {
 
 // interface
 
-bool multiLogRegInit(Data *d, Result *r) {
+bool multiLogRegInit(Data *d) {
     if (d->setup.sampleSize < 1 || d->setup.featureSize < 1)
         return false;
     d->training.x = (double**)calloc(d->setup.sampleSize, sizeof(double*) + d->setup.featureSize * sizeof(double));
@@ -89,49 +92,61 @@ bool multiLogRegInit(Data *d, Result *r) {
     for (int i = 0 ; i < d->setup.sampleSize ; ++i)
         *(d->training.x+i) = (double*)(d->training.x + d->setup.sampleSize) + i + d->setup.featureSize;
     d->training.y = (int *)calloc(d->setup.sampleSize, sizeof(int));
-    d->weight = (double *)calloc(d->setup.featureSize, sizeof(double));
-    if (!d -> training.y || !d -> weight)
+    d->weight.w = (double *)calloc(d->setup.featureSize, sizeof(double));
+    if (!d -> training.y || !d -> weight.w)
         return false;
-    d->sampleCounter = 0;
-    d->weightCounter = 0;
+    d->training.counter = 0;
+    d->weight.counter = 0;
     return true;
 }
 
 bool multiLogRegPushData(Data *d, double *x, double y) {
-	if (d->sampleCounter == d->setup.sampleSize)
+	if (d->training.counter == d->setup.sampleSize)
 		return false;
-    d->training.x[d->sampleCounter] = x;
-    d->training.y[d->sampleCounter++] = y;
+    d->training.x[d->training.counter] = x;
+    d->training.y[d->training.counter++] = y;
     return true;
 }
 
 bool multiLogRegPushWeight(Data *d, double w) {
-	if (d->weightCounter == d->setup.featureSize)
+	if (d->weight.counter == d->setup.featureSize)
 		return false;
-    *(d->weight+d -> weightCounter++) = w;
+    *(d->weight.w + d->weight.counter++) = w;
     return true;
 }
 
 bool multiLogRegAutoWeight(Data *d) {
 	srand(time(NULL));
 	for (int i = 0 ; i < d->setup.featureSize ; i++) {
-		*(d->weight + i) = calcBoXMuller();
+		*(d->weight.w + i) = calcBoXMuller();
 	}
 }
 
 bool multiLogRegTrain(Data *d) {
-    for (int iteration = 0 ; iteration < d->setup.maxIteration ; ++iteration)
-        for (int i = 0; i < d->sampleCounter; ++i) {
-            double p = calcSigmoid(calcDotProduct(d->weight, d->training.x[i], d->setup.featureSize));
+    for (int iteration = 0 ; iteration < d->setup.maxIteration ; ++iteration) {
+    	unsigned int correctPredictions = 0;
+    	double totalLoss = 0.0;
+        for (int i = 0; i < d->training.counter; ++i) {
+            double p = calcSigmoid(calcDotProduct(d->weight.w, d->training.x[i], d->setup.featureSize));        	
+			int pClass = (p >= 0.5) ? 1 : 0;
+            if (pClass == d->training.y[i]) {
+                correctPredictions++;
+            }    
+            double loss = -((d->training.y[i]) * log(p) + (1 - d->training.y[i]) * log(1 - p)); /* log-loss */
+            //double loss = -(d->training.y[i]) * log(p) - (1 - d->training.y[i]) * log(1 - p); 	/* binary cross-entropy */ 
+            totalLoss += loss;
             for (int j = 0; j < d->setup.featureSize; ++j) {
-                *(d->weight+j) += d->setup.learningRate * (d->training.y[i] - p) * d->training.x[i][j];
+                *(d->weight.w+j) += d->setup.learningRate * (d->training.y[i] - p) * d->training.x[i][j];
             }
         }
+        d->accuracy = (double)correctPredictions / d->training.counter;
+        d->loss = totalLoss/d->training.counter;
+	}
     return true;
 }
 
 int multiLogRegPredict(Data *d, double *inputData) {
-    double p = calcSigmoid(calcDotProduct(d->weight, inputData, d->setup.featureSize));
+    double p = calcSigmoid(calcDotProduct(d->weight.w, inputData, d->setup.featureSize));
     return (p >= 0.5) ? 1 : 0;
 }
 
@@ -158,17 +173,12 @@ bool multiLogRegMakeValidArray(Data * d, double ratio) {
 		}
 		push(&d->validationIndex, temp);		
 	}
-	
-	
-	
 }
 
 int main() {
 
 	// structure
     Data data;
-    //Setup setup;
-    Result result;
 
     // test datas  
     #define SAMPLES 6
@@ -182,22 +192,23 @@ int main() {
          {7.0, 5.0, 1.0},
          {2.0, 5.0, 1.0}
 		};
-
-    int Y[SAMPLES] =
+		
+	int Y[SAMPLES] =
 		{0, 0, 0, 1, 1, 1}; 
-			
+
+		
     double weights[FEATURES] =
 		{0.0,0.0,0.0}; 
 
 	// setup
-    data.setup.maxIteration = 10000;
+    data.setup.maxIteration = 1000;
     data.setup.learningRate = 0.01;
 
     data.setup.sampleSize = SAMPLES;
     data.setup.featureSize = FEATURES;
 
     // init
-    multiLogRegInit(&data, &result);
+    multiLogRegInit(&data);
 
     // push (training) datas
     for (int i = 0; i < data.setup.sampleSize ; i++)
@@ -212,15 +223,26 @@ int main() {
     multiLogRegTrain(&data);
 
     // check
-    double input_data[FEATURES] = {2.2, 4.9, 1.8};
+    double input_data[FEATURES] = {7.0, 5.0, 0.8};
 
     // predection
-    result.result = multiLogRegPredict(&data, input_data);
+    data.result = multiLogRegPredict(&data, input_data);
     
     // show the result
-    printf("The prediction is %d with the (%.1lf %.1lf %.1lf) input numbers.\n", result.result, input_data[0], input_data[1], input_data[2]);
+    printf("The prediction is %d with the (%.1lf %.1lf %.1lf) input numbers.\n", data.result, input_data[0], input_data[1], input_data[2]);
 
 	multiLogRegMakeValidArray(&data, 0.48);
 	
+	printf ("Loss: %lf, Accuracy: %lf", data.loss, data.accuracy);
+	
     return 0;
 }
+
+/*
+
+Minden minta előrejelzett osztályát meg kell határozni a tanítás után.
+Össze kell hasonlítani az előrejelzett osztályokat a valós osztályokkal.
+Kiszámítjuk a helyesen osztályozott minták számát.
+Az accuracy értéke a helyesen osztályozott minták számának és az összes minta számának hányadosa.
+
+*/
